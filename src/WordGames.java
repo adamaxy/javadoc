0aee34aa47c1fb50464fc004323c3e88335499b6
Alex <alex97av@gmail.com>
2023-07-30 10:48:36 +0200

    modifica a  WordGames :aggiunto spazio ad 'addHelloWord'

--- src/WordGames.java
+++ src/WordGames.java
@@ -1,6 +1,6 @@
 public class WordGames {
     public String addHelloWord(String wordHere){
-        return "Hello"+wordHere;
+        return "Hello "+wordHere;
     }
     public String getFullName(String name,String surname){
         return name+" "+surname;
