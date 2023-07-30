8d5b87a122a7397c84fa8c84b2823dc4317ad766
Alex <alex97av@gmail.com>
2023-07-30 10:49:32 +0200

    Creazione oggetto di WordGames ed aggiunta print dei metodi

--- src/Tester.java
+++ src/Tester.java
@@ -1,3 +1,7 @@
 public class Tester {
-
+public static void main(String[]args ){
+    WordGames wordObj=new WordGames();
+    System.out.println(wordObj.addHelloWord("Word"));
+    System.out.println(wordObj.getFullName("Alessandro","Venuti"));
+}
 }
