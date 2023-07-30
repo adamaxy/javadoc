1462d953f3e87881da64c66d55365be243145369
Alex <alex97av@gmail.com>
2023-07-30 10:37:46 +0200

    add medot: addHelloWord and getFullName

--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,3 @@
 public class Tester {
-    public String addHelloWord(String wordHere){
-        return "Hello"+wordHere;
-    }
-    public String getFullName(String name,String surname){
-        return name+" "+surname;
-    }
+
 }
--- src/WordGames.java
+++ src/WordGames.java
@@ -1,2 +1,8 @@
 public class WordGames {
+    public String addHelloWord(String wordHere){
+        return "Hello"+wordHere;
+    }
+    public String getFullName(String name,String surname){
+        return name+" "+surname;
+    }
 }
